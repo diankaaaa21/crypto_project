09e67fdcc15018d08d02ffffdded16cdf2188573
diankaaaa21 <diankapachina31@gmail.com>
2025-03-30 18:22:29 +0200

    Update tasks.py

--- crypto_app/tasks.py
+++ crypto_app/tasks.py
@@ -1,19 +1,8 @@
-import asyncio
-import json
-import logging
 from datetime import datetime
 
-import websockets
-from asgiref.sync import async_to_sync
 from celery import shared_task
 
 from .models import Trade
-
-logging.basicConfig(
-    filename="crypto.log",
-    level=logging.DEBUG,
-    format="%(asctime)s - %(levelname)s - %(message)s",
-)
 
 
 @shared_task
@@ -28,42 +17,14 @@ def save_trade(symbol, price, quantity, trade_time):
     trade.save()
 
 
-async def fetch_trades(symbol):
-    url = f"wss://stream.binance.com:9443/ws/{symbol.lower()}@trade"
-
-    while True:
-        try:
-            async with websockets.connect(url, ping_interval=30) as websocket:
-                logging.info(f"Connected to {url}")
-
-                while True:
-                    data = await websocket.recv()
-                    data = json.loads(data)
-
-                    save_trade.delay(
-                        symbol=data["s"],
-                        price=data["p"],
-                        quantity=data["q"],
-                        trade_time=data["T"],
-                    )
-
-                    logging.debug(f"The data is saved: {data}")
-
-        except websockets.exceptions.ConnectionClosed:
-            logging.warning(f"Connection with {symbol} interrupted. Restarting...")
-            await asyncio.sleep(5)
-
-        except Exception as e:
-            logging.critical(
-                f"Error while retrieving data from  {symbol}: {e}", exc_info=True
-            )
-            await asyncio.sleep(10)
-
-
 @shared_task
 def start_binance():
+    import asyncio
+
+    from .binance_ws import fetch_trades
+
     symbols = ["btcusdt", "ethusdt", "bnbusdt"]
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    loop.run_until_complete(asyncio.gather(*(fetch_trades(symbol) for symbol in symbols)))
+    loop = asyncio.get_event_loop()
+    for symbol in symbols:
+        loop.create_task(fetch_trades(symbol))
 
